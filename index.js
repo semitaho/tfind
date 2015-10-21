4eaf9a9b4615b30b9f273b0f1926dc263f32395c
Toni Aho <toni.aho@siili.fi>
2015-10-22 00:28:01 +0300

    new files

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var express = require('express');
-var app = express();
-
+var app = module.exports = express();
+//app.use(app.router);
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res){
+  res.send('<h1>kukka</h1>');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+app.listen(app.get('port'),function(){
+  console.log('up and running...');
 });
