915ce7b40977b9ddc550d1d529630d83b8bcc909
Toni Aho <toni.aho@siili.fi>
2015-10-22 22:23:07 +0300

    new files

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = module.exports = express();
 //app.use(app.router);
 app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+// Set our default template engine to "jade"
+// which prevents the need for extensions
+// (although you can still mix and match)
+app.set('view engine', 'jade');
 
-app.get('/', function(req, res){
-  res.send('<h1>kukka</h1>');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
 
-app.listen(app.get('port'),function(){
+app.listen(app.get('port'), function () {
   console.log('up and running...');
 });
