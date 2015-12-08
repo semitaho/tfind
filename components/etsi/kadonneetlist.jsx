8ca227e1fa646453eb57564a05f8c9c3e840b701
Toni Aho <toni.aho@siili.fi>
2015-12-08 23:34:22 +0200

    kadonneet

---
+++ components/etsi/kadonneetlist.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import KadonneetMap from './kadonneetSearchMap.jsx';
+
+class KadonneetList extends React.Component{
+  constructor(){
+    super();
+    this.state = {item: null};
+    this.closeMap = this.closeMap.bind(this);
+  }
+
+  clickButton(item){
+    console.log('item', item);
+    this.setState({item: item});
+  }
+
+  closeMap(){
+    this.setState({item:null});
+  };
+
+  render(){
+    return <div>
+            <div className="list-group">
+              {this.props.items.map((item,key)=> {
+                  return <button key={'search_'+key} className="list-group-item" onClick={this.clickButton.bind(this,item)}>  {item.name}<span className="pull-right glyphicon glyphicon-menu-right" aria-hidden="true"></span></button>;
+              })}
+            </div>  
+            {this.state.item !== null ?  <KadonneetMap item={this.state.item} onclose={this.closeMap}  /> : ''}
+          </div>
+  }
+
+}
+
+export default KadonneetList;
